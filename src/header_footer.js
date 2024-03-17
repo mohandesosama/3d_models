4bd8740f5c8c6dbde842d826b26faf4a3ec6c3bf
mohandesosama <mohandesosama@yahoo.com>
2024-03-18 00:49:47 +0400

    fixed the login logout with useremail displayed when there is a user, and welcome message is displayed when no user logged in

--- src/header_footer.js
+++ src/header_footer.js
@@ -4,9 +4,9 @@ function fetchPartial(url, callback) {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4) {
             if (xhr.status === 200) {
-                callback(null, xhr.responseText);
+                callback(null, xhr.responseText); // Call the callback function with the responseText
             } else {
-                callback(new Error('Failed to fetch partial: ' + xhr.status), null);
+                callback(new Error('Failed to fetch partial: ' + xhr.status), null); // Call the callback function with an error
             }
         }
     };
@@ -16,10 +16,10 @@ function fetchPartial(url, callback) {
 
 // Function to render header, footer, and other content
 function renderPageContent() {
-    //this part of the code is added becasue index page is put outside
+    //this part of the code is added because the index page is put outside
     //the page folder which contains all pages.
-    var header_page_path = indexPage() ? 'pages/header.html' : 'header.html';
-    var footer_page_path = indexPage() ? 'pages/footer.html' : 'footer.html';
+    var header_page_path = isIndexPage() ? 'pages/header.html' : 'header.html';
+    var footer_page_path = isIndexPage() ? 'pages/footer.html' : 'footer.html';
 
     // Fetch header partial
     fetchPartial(header_page_path, function (error, headerHtml) {
@@ -46,16 +46,54 @@ function renderPageContent() {
             // Render footer template
             var footerTemplate = Handlebars.compile('<footer>{{> footer}}</footer>');
             document.querySelector('footer').innerHTML = footerTemplate();
+
+            // After rendering header and footer, render the user content
+            renderUserContent();
         });
     });
     //check if you are calling from the index page or other pages. 
-    indexPage() ? addCSS('css/header_footer.css') : addCSS('../css/header_footer.css');
+    isIndexPage() ? addCSS('css/header_footer.css') : addCSS('../css/header_footer.css');
 }
-function indexPage() {
+
+// Function to render user content
+function renderUserContent() {
+    const user_email = localStorage.getItem("email");
+    const userElement = document.getElementById("user_email");
+    const loginLink = document.getElementById("loginLink");
+
+    console.log(user_email);
+
+    if (userElement) {
+        if (user_email === "" || user_email === null) {
+            userElement.innerText = "Welcome guest";
+            if (loginLink) {
+                loginLink.innerText = "Login";
+                loginLink.href = "../pages/login.html"; // Set the href attribute to the login page
+            }
+        } else {
+            userElement.innerText = user_email;
+            if (loginLink) {
+                loginLink.innerText = "Logout";
+                loginLink.onclick = logout; // Add a JavaScript function call for logout
+            }
+        }
+    } else {
+        console.error("User element not found");
+    }
+}
+
+// Logout function
+function logout() {
+    localStorage.clear();
+    window.location.href = "../index.html"; // Redirect to the main page
+}
+
+
+function isIndexPage() {
     // Get the path of the current URL
     var path = window.location.pathname;
 
-    return path.endsWith("index.html") ? true:false;
+    return path.endsWith("index.html") ? true : false;
 }
 
 //instaead of adding a line at the beginning of each new page
@@ -70,5 +108,3 @@ function addCSS(cssFile) {
 
 // Render header, footer, and other content
 renderPageContent();
-
-
