a78e3bf488cfa20d7a7aef7ee51571745d5b381c
mohandesosama <mohandesosama@yahoo.com>
2024-03-28 18:32:55 +0400

    updated the contact me with flash feedback

--- src/contact_me.js
+++ src/contact_me.js
@@ -18,15 +18,37 @@ const db = getFirestore(app);
 
 // Function to send a message
 export function sendMessage(message) {
+    const msgBtn = document.getElementById("sendMessageBtn");
+    const textArea = document.getElementById("message");
+    const flashMessage = document.getElementById("flashMessage");
+
+    msgBtn.disabled = true;
+    textArea.disabled = true;
+    msgBtn.style.opacity = 0.5;
+    textArea.style.opacity = 0.5;
+
     addDoc(collection(db, 'messages'), {
         text: message
     })
         .then(function (docRef) {
             console.log("Message added with ID: ", docRef.id);
+            textArea.value = "";
+            //this part for flash message
+            flashMessage.style.display = 'block';
+            setTimeout(function () {
+                flashMessage.style.display = 'none';
+            }, 3000);
+           
         })
         .catch(function (error) {
             console.error("Error adding message: ", error);
         });
+
+    msgBtn.disabled = false;
+    textArea.disabled = false;
+    msgBtn.style.opacity = 1;
+    textArea.style.opacity = 1;
+
 }
 
 // Function to display messages
@@ -42,20 +64,23 @@ export function displayMessages() {
         });
     });
 }
-const textarea=document.getElementById("message");
-const charcount=document.getElementById("char-count");
-textarea.addEventListener('input',function(){
-    const maxLength=parseInt(textarea.getAttribute("maxlength"));
-    const currentLength=textarea.value.length;
-    charcount.textContent=currentLength + " / " +maxLength;
 
-    if(currentLength > maxLength)
-    {
-        charcount.style.color="red";
-        textarea.value=textarea.value.substring(0,maxLength);
+//this part for character count
+const textarea = document.getElementById("message");
+const charcount = document.getElementById("char-count");
+textarea.addEventListener('input', function () {
+    const maxLength = parseInt(textarea.getAttribute("maxlength"));
+    const currentLength = textarea.value.length;
+    charcount.textContent = currentLength + " / " + maxLength;
+
+    if (currentLength > maxLength) {
+        charcount.style.color = "red";
+        textarea.value = textarea.value.substring(0, maxLength);
     }
-    else{
-        charcount.style.color="gray";
+    else {
+        charcount.style.color = "gray";
     }
 });
+
+
 
